6afd39c779112f4c4a4cb27b632d96dbc36543fe
Fernando Ambrosio <fer22ay@gmail.com>
2020-01-05 20:03:47 -0600

    Interfaz IVisualizable con metodos obligatorios de las clases que los implemente

---
+++ src/com/avanzadose/model/IVisualizable.java
@@ -0,0 +1,19 @@
+package com.avanzadose.model;
+import java.util.Date;
+
+public interface IVisualizable {
+    
+    /***
+     * Este metodo captura el tiempo exacto de inicio de visualizacion
+     * @param dateI Es un objeto {@code Date} con el tiempo de inicio exacto
+     * @return Devuelve la fecha y hora capturada.
+     */
+    Date startToSee(Date dateI);
+    
+    /***
+     * Este metodo captura el tiempo exacto de inicio y final de visualizacion
+     * @param dateI Es un objeto {@code Date} con el tiempo de inicio exacto
+     * @param dateF Es un objeto {@code Date} con el tiempo de final exacto
+     */
+    void stopToSee(Date dateI, Date dateF);
+}
