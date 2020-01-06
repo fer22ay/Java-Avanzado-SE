412b9873e186c2e959f839a52012c6f5b6f109fc
Fernando Ambrosio <fer22ay@gmail.com>
2020-01-05 20:05:17 -0600

    Clase Magazine v0.1.0

---
+++ src/com/avanzadose/model/Magazine.java
@@ -0,0 +1,31 @@
+package com.avanzadose.model;
+
+import java.util.Date;
+
+public class Magazine extends Publication{
+    
+    private int id;
+
+    public Magazine(String title, Date editionDate, String editorial) {
+        super(title, editionDate, editorial);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "\n :: MAGAZINE ::"
+                + "\n Title: " + getTitle() + 
+                "\n Editorial: " + getEditorial() + 
+                "\n Edition Date: " + getEditionDate();
+    }
+    
+    
+    
+}
